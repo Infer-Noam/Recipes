200c6042bd1777fc3bd2b6974dae8c05f8fb8766
Infer-Noam <xd.NNoam39@idf.il>
2025-07-21 12:17:58 +0300

    Made the recipe deletion into an hook

--- frontend/src/pages/home/homePage.tsx
+++ frontend/src/pages/home/homePage.tsx
@@ -1,25 +1,21 @@
 import { Recipe } from "../../components/recipe/recipe";
 import { Box, Grid } from "@mui/material";
 import { useGetRecipes } from "../../hooks/api/useGetRecipes.api";
-import api from "../../api";
-import { type DeleteRecipeReq } from "../../../../shared/http-types/recipe/deleteRecipe.http-type";
-import { useQueryClient } from "@tanstack/react-query";
 import Styles from "./homePage.style";
-import { USE_GET_RECIPES_KEY } from "../../hooks/api/useGetRecipes.api";
-import { chefSrcArray } from "../../consts/chefSrcArray.const";
-import type { FC } from "react";
+import { useDeleteRecipe } from "../../hooks/api/useDeleteRecipe.api";
 
-const HomePage: FC = () => {
+const HomePage = () => {
   const { data: recipes } = useGetRecipes();
 
-  const queryClient = useQueryClient();
+  const { mutate: deleteRecipe } = useDeleteRecipe();
 
-  const deleteRecipe = async (uuid: string) => {
-    await api.delete<DeleteRecipeReq>("/recipe", { data: { uuid } });
-    queryClient.invalidateQueries({ queryKey: [USE_GET_RECIPES_KEY] });
-  };
+  const chefSrcArray = [227920, 240413, 227924, 44252, 33158, 218334].map(
+    (n) => {
+      return `https://www.svgrepo.com/show/${n}/chef.svg`;
+    }
+  );
 
-  const getRandomChefSrc = () =>
+  const chefSrc = () =>
     chefSrcArray[Math.floor(Math.random() * chefSrcArray.length)];
 
   if (recipes) {
@@ -38,7 +34,7 @@ const HomePage: FC = () => {
                 deleteRecipe={() => {
                   deleteRecipe(recipe.uuid);
                 }}
-                chefAvatarSrc={getRandomChefSrc()}
+                chefAvatarSrc={chefSrc()}
               />
             </Grid>
           ))}
