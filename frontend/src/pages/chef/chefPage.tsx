3ea13fef87e90d5d8916f725b2ca3888627ecb4c
Infer-Noam <xd.NNoam39@idf.il>
2025-07-24 09:21:51 +0300

    Added the basic things for chef page

--- frontend/src/pages/chef/chefPage.tsx
+++ frontend/src/pages/chef/chefPage.tsx
@@ -1,10 +1,17 @@
 import { Box, Typography } from "@mui/material";
+import { useDeleteChef } from "../../hooks/api/useDeleteChef.api";
+import { useGetChefs } from "../../hooks/api/useGetChefs.api";
+import { useSaveChef } from "../../hooks/api/useSaveChef.api";
 import type { FC } from "react";
 
-const ChefPage: FC = () => (
-  <Box>
-    <Typography>Chef</Typography>
-  </Box>
-);
+const ChefPage: FC = () => {
+  const { data: chefs } = useGetChefs();
+  const { mutate: deleteChef } = useDeleteChef();
+  const { mutateAsync: saveChef } = useSaveChef();
+
+  if (chefs) {
+    return <Box>{chefs.map((c) => JSON.stringify(c))}</Box>;
+  } else return null;
+};
 
 export default ChefPage;
