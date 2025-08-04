9600af07f70a7c9465b88847ec659e8d550ddb17
Infer-Noam <xd.NNoam39@idf.il>
2025-08-04 11:03:09 +0300

    Applied suggestions

--- frontend/src/pages/chef/ChefPage.tsx
+++ frontend/src/pages/chef/ChefPage.tsx
@@ -1,40 +1,31 @@
 import { useDeleteChef } from "../../hooks/api/useDeleteChef.api";
 import { useGetChefs } from "../../hooks/api/useGetChefs.api";
 import { useSaveChef } from "../../hooks/api/useSaveChef.api";
-import ChefTable from "../../components/chefTable/chefTable";
+import ChefTable from "../../components/chefTable/ChefTable";
 import { type FC } from "react";
-import { isAxiosError } from "axios";
-import swal from "sweetalert";
 import BackdropLoading from "../../components/backdropLoading/BackdropLoading";
 import CentralErrorAlert from "../../components/centralErrorAlert/CentralErrorAlert";
+import { useSwal } from "../../hooks/useSwal";
 
 const ChefPage: FC = () => {
   const { data: chefs, isLoading } = useGetChefs();
 
-  const displayError = (err: unknown) => {
-    let text = "";
-    if (isAxiosError(err))
-      text = err.response?.data?.message || "Something went wrong";
-    else text = "Something went wrong";
-    swal("Error", text, "error");
-  };
+  const { showError, showSuccess } = useSwal();
 
-  const { mutateAsync: deleteChef } = useDeleteChef(displayError, (data) => {
-    swal("Success", data.message, "success");
+  const { mutateAsync: deleteChef } = useDeleteChef(showError, (data) => {
+    showSuccess(data.message);
   });
-  const { mutateAsync: saveChef } = useSaveChef(displayError, (data) => {
-    swal("Success", data.message, "success");
+  const { mutateAsync: saveChef } = useSaveChef(showError, (data) => {
+    showSuccess(data.message);
   });
 
   if (isLoading) return <BackdropLoading />;
 
-  if (chefs) {
-    return (
-      <ChefTable chefs={chefs} deleteChef={deleteChef} saveChef={saveChef} />
-    );
-  }
+  if (!chefs) return <CentralErrorAlert text="Something went wrong..." />;
 
-  return <CentralErrorAlert text="Something went wrong..." />;
+  return (
+    <ChefTable chefs={chefs} deleteChef={deleteChef} saveChef={saveChef} />
+  );
 };
 
 export default ChefPage;
