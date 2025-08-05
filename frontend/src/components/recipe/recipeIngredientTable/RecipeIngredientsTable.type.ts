37da5e7ef50ce74771ce7f295dec188c50b21d11
Infer-Noam <xd.NNoam39@idf.il>
2025-08-05 21:12:03 +0300

    Deleted some files

--- frontend/src/components/recipe/recipeIngredientTable/RecipeIngredientsTable.type.ts
+++
@@ -1,8 +0,0 @@
-import { type Ingredient as IngredientModel } from "../../../../../shared/types/ingredient.type";
-import { type Control } from "react-hook-form";
-import type { RecipeFormData } from "../Recipe.type";
-
-export type RecipeIngredientsTableProps = {
-  ingredients: IngredientModel[];
-  control: Control<RecipeFormData, unknown, RecipeFormData>;
-};
