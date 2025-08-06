d3dadbd918cbf571743ee77c61858a6f084cd02f
Infer-Noam <xd.NNoam39@idf.il>
2025-08-06 11:16:30 +0300

    Deleted files

--- frontend/src/components/recipe/items/recipeIngredientTable/elements/error/error.style.ts
+++
@@ -1,7 +0,0 @@
-import type { SxProps } from "@mui/material";
-
-const typography: SxProps = { mt: 1, display: "block" };
-
-export default {
-  typography,
-};
--- frontend/src/components/recipe/items/recipeIngredientTable/elements/measurementUnit/measurementUnit.style.ts
+++
@@ -1,7 +0,0 @@
-import type { SxProps } from "@mui/material";
-
-const measurementUnitSelect: SxProps = { width: "110px" };
-
-export default {
-  measurementUnitSelect,
-};
