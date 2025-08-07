16dd92a0c4aa5ecd1357f55b50b1e315b4d86608
Infer-Noam <xd.NNoam39@idf.il>
2025-08-07 11:06:47 +0300

    Added ingredient element

---
+++ frontend/src/components/recipe/items/recipeIngredientTable/elements/ingredient/IngredientElement.tsx
@@ -0,0 +1,60 @@
+import { Autocomplete, Box, TextField } from "@mui/material";
+import type { FC } from "react";
+import Styles from "./ingredientElement.style";
+import type { Ingredient as IngredientModel } from "../../../../../../../../shared/types/ingredient.type";
+import type { RecipeFormData } from "../../../../Recipe.type";
+import { Controller, useFormContext } from "react-hook-form";
+
+type IngredientElementProps = {
+  index: number;
+  ingredients: IngredientModel[];
+};
+
+const IngredientElement: FC<IngredientElementProps> = ({
+  index,
+  ingredients,
+}) => {
+  const { control } = useFormContext<RecipeFormData>();
+  return (
+    <Controller
+      name={`ingredients.${index}.ingredient`}
+      control={control}
+      render={({ field, fieldState: { error } }) => {
+        const getIngredient = (ingredinetUuid?: string) =>
+          ingredients.find((ingredient) => ingredient.uuid === ingredinetUuid);
+
+        const ingredient = getIngredient(field.value.uuid);
+
+        return (
+          <Box sx={Styles.ingredientAutocompleteBox}>
+            <Autocomplete
+              sx={Styles.ingredientAutocomplete}
+              value={ingredient?.name}
+              onChange={(_: any, newValue: string | null) => {
+                const selectedIngredient = ingredients.find(
+                  (ingredient) => ingredient.name === newValue
+                );
+                field.onChange(
+                  selectedIngredient
+                    ? { uuid: selectedIngredient.uuid }
+                    : undefined
+                );
+              }}
+              disableClearable={true}
+              options={ingredients.map((ingredient) => ingredient.name)}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  error={!!error}
+                  helperText={error?.message}
+                />
+              )}
+            />
+          </Box>
+        );
+      }}
+    />
+  );
+};
+
+export default IngredientElement;
---
+++ frontend/src/components/recipe/items/recipeIngredientTable/elements/ingredient/ingredientElement.style.ts
@@ -0,0 +1,15 @@
+import type { SxProps } from "@mui/material";
+
+const ingredientAutocomplete: SxProps = {
+  width: "175px",
+};
+
+const ingredientAutocompleteBox: SxProps = {
+  display: "flex",
+  justifyContent: "center",
+};
+
+export default {
+  ingredientAutocomplete,
+  ingredientAutocompleteBox,
+};
