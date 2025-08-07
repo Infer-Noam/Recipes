1910f4e8936e916bd565384ea5422852eb9c80a7
Infer-Noam <xd.NNoam39@idf.il>
2025-08-07 11:08:23 +0300

    Addedd recipe ingredient table item type and const

---
+++ frontend/src/components/recipe/items/recipeIngredientTable/recipeIngredientsTableItem.const.ts
@@ -0,0 +1,9 @@
+import { MeasurementUnit } from "../../../../../../shared/enums/measurement-unit.enum";
+
+export const DEFAULT_RECIPE_INGREDIENT_DETAILS = {
+  recipe: undefined,
+  ingredient: undefined,
+  amount: 1,
+  measurementUnit: MeasurementUnit.KG,
+};
+export const HEADER_LABELS = ["Ingredient", "Amount", "Measurement unit"];
