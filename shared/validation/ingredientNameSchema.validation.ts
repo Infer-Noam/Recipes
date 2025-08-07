f874a666f7f5927765619ddeef4de6c4f42b28b0
Infer-Noam <xd.NNoam39@idf.il>
2025-08-07 10:51:33 +0300

    Added zod validation schemas

--- shared/validation/ingredientNameSchema.validation.ts
+++ shared/validation/ingredientNameSchema.validation.ts
@@ -1,5 +1,8 @@
 import { z } from "zod";
 
 export const IngredientNameSchema = z.object({
-  name: z.string().min(1).max(15),
+  name: z
+    .string()
+    .min(1, "Ingredient name is required")
+    .max(15, "Ingredient name must be at most 15 characters"),
 });
