50ae00757318ce1505a2fef1cc436e9100d7ae7c
Infer-Noam <xd.NNoam39@idf.il>
2025-08-07 10:44:08 +0300

    Added controlled text field

---
+++ frontend/src/components/controlledTextField/ControlledTextField.tsx
@@ -0,0 +1,43 @@
+import {
+  Controller,
+  useFormContext,
+  type FieldValues,
+  type Path,
+} from "react-hook-form";
+import { TextField, type TextFieldProps } from "@mui/material";
+
+type ControlledTextFieldProps<T extends FieldValues> = TextFieldProps & {
+  name: Path<T>;
+  transformValue?: (value: string) => any;
+};
+
+const ControlledTextField = <T extends FieldValues>({
+  name,
+  transformValue,
+  ...rest
+}: ControlledTextFieldProps<T>) => {
+  const { control } = useFormContext();
+
+  return (
+    <Controller
+      name={name}
+      control={control}
+      render={({ field, fieldState: { error } }) => (
+        <TextField
+          variant="outlined"
+          error={!!error}
+          helperText={error?.message}
+          {...field}
+          {...rest}
+          onChange={(e) => {
+            const value = e.target.value;
+            const transformedValue = transformValue?.(value) ?? value;
+            field.onChange(transformedValue);
+          }}
+        />
+      )}
+    />
+  );
+};
+
+export default ControlledTextField;
