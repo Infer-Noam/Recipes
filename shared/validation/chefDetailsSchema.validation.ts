6e2bf9e8af62ab5bd8c360000c060b419c438e25
Infer-Noam <xd.NNoam39@idf.il>
2025-08-10 17:12:13 +0300

    Changed schemas

--- shared/validation/chefDetailsSchema.validation.ts
+++ shared/validation/chefDetailsSchema.validation.ts
@@ -1,19 +1,22 @@
 import { EMAIL_REGEX, PHONE_REGEX } from "../consts/regex.const";
 import { z } from "zod";
 
-export const ChefDetailsSchema = z.object({
-  uuid: z.string().optional(),
-  firstName: z
-    .string()
-    .min(1, "First name is required")
-    .max(20, "First name must be at most 20 characters"),
-  lastName: z
-    .string()
-    .min(1, "Last name is required")
-    .max(20, "Last name must be at most 20 characters"),
-  phone: z
-    .string()
-    .regex(PHONE_REGEX, "Phone number must be valid")
-    .length(10, "Phone number must be 10 digits"),
-  email: z.string().regex(EMAIL_REGEX, "Email must be valid"),
-});
+export const ChefDetailsSchema = z.object(
+  {
+    uuid: z.uuid().optional(),
+    firstName: z
+      .string()
+      .min(1, "First name is required")
+      .max(20, "First name must be at most 20 characters"),
+    lastName: z
+      .string()
+      .min(1, "Last name is required")
+      .max(20, "Last name must be at most 20 characters"),
+    phone: z
+      .string()
+      .regex(PHONE_REGEX, "Phone number must be valid")
+      .length(10, "Phone number must be 10 digits"),
+    email: z.string().regex(EMAIL_REGEX, "Email must be valid"),
+  },
+  "Chef is required"
+);
--- shared/validation/ingredientNameSchema.validation.ts
+++ shared/validation/ingredientNameSchema.validation.ts
@@ -1,8 +1,10 @@
 import { z } from "zod";
 
+export const IngredientNameFieldSchema = z
+  .string()
+  .min(1, "Ingredient name is required")
+  .max(15, "Ingredient name must be at most 15 characters");
+
 export const IngredientNameSchema = z.object({
-  name: z
-    .string()
-    .min(1, "Ingredient name is required")
-    .max(15, "Ingredient name must be at most 15 characters"),
+  name: IngredientNameFieldSchema,
 });
