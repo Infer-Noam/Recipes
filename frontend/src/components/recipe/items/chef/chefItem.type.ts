dadbce3f743192458ca19486ad38d4a73a429271
Noam Naor <145200566+Infer-Noam@users.noreply.github.com>
2025-08-10 10:34:51 +0300

    Revert "Added recipe items"

--- frontend/src/components/recipe/items/chef/chefItem.type.ts
+++
@@ -1,4 +0,0 @@
-import { z } from "zod"
-import { ChefDetailsSchema } from "../../../../../../shared/validation/chefDetailsSchema.validation"
-
-export type ChefFormData = z.infer<typeof ChefDetailsSchema>
