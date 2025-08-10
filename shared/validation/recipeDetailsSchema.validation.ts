fddaf19ad28b1f1957ee9782837daa9cc1babe85
Infer-Noam <xd.NNoam39@idf.il>
2025-08-10 11:18:27 +0300

    Made recipe details validation schema use full chef and ingredient details and changed affected code.

--- shared/validation/recipeDetailsSchema.validation.ts
+++ shared/validation/recipeDetailsSchema.validation.ts
@@ -1,47 +1,22 @@
 import { z } from "zod";
-import { MeasurementUnit } from "../enums/measurement-unit.enum";
 import { IMAGE_URL_REGEX } from "../consts/regex.const";
-import { UuidSchema } from "./uuidSchema.validation";
+import { RecipeIngredientSchema } from "./recipeIngredientSchema.validation";
+import { ChefDetailsSchema } from "./chefDetailsSchema.validation";
+import { RecipeStepSchema } from "./recipeStepSchema.validation";
 
 export const RecipeDetailsSchema = z.object({
-  uuid: z.string().optional(),
+  uuid: z.uuid().optional(),
   name: z
     .string()
     .min(1, "Recipe name is required")
     .max(20, "Recipe name must be at most 20 characters"),
   steps: z
-
-    .array(
-      z.object({
-        uuid: z.string().optional(),
-        recipe: UuidSchema.optional(),
-        placement: z.number(),
-        text: z.string().min(1, "Each step must have content"),
-      })
-    )
+    .array(RecipeStepSchema)
     .min(1, "At least one step is required")
     .max(20, "You can add up to 20 steps only"),
-  chef: z.object(
-    {
-      uuid: z.string(),
-    },
-    "Chef is required"
-  ),
+  chef: ChefDetailsSchema,
   ingredients: z
-    .array(
-      z.object({
-        uuid: z.string().optional(),
-        recipe: UuidSchema.optional(),
-        ingredient: UuidSchema.refine((val) => val, {
-          message: "Ingredient is required",
-        }),
-        amount: z.number().min(1, "Amount must be at least 1"),
-        measurementUnit: z.enum(
-          MeasurementUnit,
-          "Measurement unit is required"
-        ),
-      })
-    )
+    .array(RecipeIngredientSchema)
     .min(1, "At least one ingredient is required")
     .max(50, "You can add up to 50 ingredients only"),
   description: z.string(),
