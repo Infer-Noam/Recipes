475aa3e0f81886dbfe3ee504b8a0539bafd53345
Infer-Noam <xd.NNoam39@idf.il>
2025-08-10 10:57:24 +0300

    Added steps entity instead of simple step array

---
+++ backend/src/recipe/recipe-step/recipeStep.entity.ts
@@ -0,0 +1,16 @@
+import { Entity, Column, ManyToOne, JoinColumn } from "typeorm";
+import { AuditEntity } from "../../audit.entity";
+import { Recipe } from "../recipe.entity";
+
+@Entity()
+export class RecipeStep extends AuditEntity {
+  @Column({ type: "integer" })
+  placement: number;
+
+  @ManyToOne(() => Recipe, (recipe) => recipe.steps, {
+    cascade: ["insert", "update", "soft-remove"],
+    orphanedRowAction: "soft-delete",
+  })
+  @JoinColumn({ name: "recipe_uuid" })
+  recipe: Recipe;
+}
