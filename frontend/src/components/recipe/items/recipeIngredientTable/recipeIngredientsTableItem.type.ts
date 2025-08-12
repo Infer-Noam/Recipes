e8f4d4fd30a5758474667d5705e40249e4b8a4f9
Infer-Noam <xd.NNoam39@idf.il>
2025-08-12 11:46:07 +0300

    Added missing type

--- frontend/src/components/recipe/items/recipeIngredientTable/recipeIngredientsTableItem.type.ts
+++ frontend/src/components/recipe/items/recipeIngredientTable/recipeIngredientsTableItem.type.ts
@@ -1,5 +1,9 @@
 import { type Ingredient as IngredientModel } from "@shared/types/ingredient.type";
+import type { RecipeIngredientSchema } from "@shared/validation/recipeIngredientSchema.validation";
+import { z } from "zod";
 
 export type RecipeIngredientsTableItemProps = {
   ingredients: IngredientModel[];
 };
+
+export type RecipeIngredientFormData = z.infer<typeof RecipeIngredientSchema>;
