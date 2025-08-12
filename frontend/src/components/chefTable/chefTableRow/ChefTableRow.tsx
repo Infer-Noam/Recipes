5e3c18e4001a479b85d20bbfdca4fbddad73dbf5
Infer-Noam <xd.NNoam39@idf.il>
2025-08-12 16:11:42 +0300

    Added a delete chef dialog and slot props for limiting text fields in chef row

--- frontend/src/components/chefTable/chefTableRow/ChefTableRow.tsx
+++ frontend/src/components/chefTable/chefTableRow/ChefTableRow.tsx
@@ -1,6 +1,6 @@
 import { IconButton, TableCell, TableRow } from "@mui/material";
 import type { ChefDetails } from "@shared/types/chef.type";
-import { type FC } from "react";
+import { Fragment, type FC } from "react";
 import Styles from "./chefTableRow.style";
 import CheckIcon from "@mui/icons-material/Check";
 import AddIcon from "@mui/icons-material/Add";
@@ -10,6 +10,8 @@ import { ChefDetailsSchema } from "@shared/validation/chefDetailsSchema.validati
 import type { ChefFormData, ChefTableRowProps } from "./chefTableRow.type";
 import ControlledTextField from "../../../components/controlledTextField/ControlledTextField";
 import { FormProvider, useForm } from "react-hook-form";
+import useToggle from "../../../hooks/useToggle";
+import DeleteChefDialog from "./DeleteChefDialog";
 
 const ChefTableRow: FC<ChefTableRowProps> = ({
   chef,
@@ -27,6 +29,12 @@ const ChefTableRow: FC<ChefTableRowProps> = ({
     reset,
   } = methods;
 
+  const {
+    open: deleteDialog,
+    handleClose: closeDeleteDialog,
+    handleOpen: openDeleteDialog,
+  } = useToggle();
+
   const onSubmit = async (chef: ChefDetails) => {
     await saveChef(chef, {
       onSuccess: () => reset(chef),
@@ -34,36 +42,51 @@ const ChefTableRow: FC<ChefTableRowProps> = ({
   };
 
   return (
-    <FormProvider {...methods}>
-      <TableRow sx={Styles.chefTableRow}>
-        <TableCell sx={Styles.centerAlign}>
-          <IconButton onClick={deleteChef}>
-            <RemoveIcon />
-          </IconButton>
-        </TableCell>
-        <TableCell sx={Styles.centerAlign}>
-          <ControlledTextField
-            name="firstName"
-            sx={Styles.firstNameTextField}
-          />
-        </TableCell>
-        <TableCell sx={Styles.centerAlign}>
-          <ControlledTextField name="lastName" sx={Styles.lastNameTextField} />
-        </TableCell>
-        <TableCell sx={Styles.centerAlign}>
-          <ControlledTextField name="email" sx={Styles.emailTextField} />
-        </TableCell>
-        <TableCell sx={Styles.centerAlign}>
-          <ControlledTextField name="phone" sx={Styles.phoneTextField} />
-        </TableCell>
-
-        <TableCell>
-          <IconButton onClick={handleSubmit(onSubmit)} disabled={!isDirty}>
-            {chef.uuid ? <CheckIcon /> : <AddIcon />}
-          </IconButton>
-        </TableCell>
-      </TableRow>
-    </FormProvider>
+    <Fragment>
+      <FormProvider {...methods}>
+        <TableRow sx={Styles.chefTableRow}>
+          <TableCell sx={Styles.centerAlign}>
+            <IconButton onClick={openDeleteDialog}>
+              <RemoveIcon />
+            </IconButton>
+          </TableCell>
+          <TableCell sx={Styles.centerAlign}>
+            <ControlledTextField
+              name="firstName"
+              sx={Styles.firstNameTextField}
+              slotProps={Styles.nameSlotProps}
+            />
+          </TableCell>
+          <TableCell sx={Styles.centerAlign}>
+            <ControlledTextField
+              name="lastName"
+              sx={Styles.lastNameTextField}
+              slotProps={Styles.nameSlotProps}
+            />
+          </TableCell>
+          <TableCell sx={Styles.centerAlign}>
+            <ControlledTextField name="email" sx={Styles.emailTextField} />
+          </TableCell>
+          <TableCell sx={Styles.centerAlign}>
+            <ControlledTextField
+              name="phone"
+              sx={Styles.phoneTextField}
+              slotProps={Styles.phoneSlotProps}
+            />
+          </TableCell>
+          <TableCell>
+            <IconButton onClick={handleSubmit(onSubmit)} disabled={!isDirty}>
+              {chef.uuid ? <CheckIcon /> : <AddIcon />}
+            </IconButton>
+          </TableCell>
+        </TableRow>
+      </FormProvider>
+      <DeleteChefDialog
+        deleteDialog={deleteDialog}
+        closeDeleteDialog={closeDeleteDialog}
+        deleteChef={deleteChef}
+      />
+    </Fragment>
   );
 };
 
--- frontend/src/components/chefTable/chefTableRow/chefTableRow.style.ts
+++ frontend/src/components/chefTable/chefTableRow/chefTableRow.style.ts
@@ -1,4 +1,5 @@
 import type { SxProps } from "@mui/material";
+import { LETTERS_REGEX, NUMBER_REGEX } from "@shared/consts/regex.const";
 
 const chefTableRow: SxProps = {
   "&:last-child td, &:last-child th": { border: 0 },
@@ -19,6 +20,30 @@ const centerAlign: SxProps = {
   verticalAlign: "top",
 };
 
+const nameSlotProps = {
+  input: {
+    inputProps: {
+      onInput: (e: React.FormEvent<HTMLInputElement>) => {
+        e.currentTarget.value = e.currentTarget.value.replace(
+          LETTERS_REGEX,
+          ""
+        );
+      },
+    },
+  },
+};
+
+const phoneSlotProps = {
+  input: {
+    inputProps: {
+      maxLength: 10,
+      onInput: (e: React.FormEvent<HTMLInputElement>) => {
+        e.currentTarget.value = e.currentTarget.value.replace(NUMBER_REGEX, "");
+      },
+    },
+  },
+};
+
 export default {
   chefTableRow,
   firstNameTextField,
@@ -26,4 +51,6 @@ export default {
   emailTextField,
   phoneTextField,
   centerAlign,
+  nameSlotProps,
+  phoneSlotProps,
 };
