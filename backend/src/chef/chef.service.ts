514108edb909fa9348ba11ab0bd260e758d7bfe1
Infer-Noam <xd.NNoam39@idf.il>
2025-08-12 16:09:38 +0300

    Made chef duplicate values have more infromed error message

--- backend/src/chef/chef.service.ts
+++ backend/src/chef/chef.service.ts
@@ -2,19 +2,34 @@ import { Recipe } from "src/recipe/recipe.entity";
 import { Chef } from "../chef/chef.entity";
 import { AppDataSource } from "../data-source";
 import { ChefDetails } from "@shared/types/chef.type";
+import { QueryFailedError } from "typeorm";
+import { HttpError } from "@shared/types/httpError.type";
 
 const chefRepository = AppDataSource.getRepository(Chef);
 
 const saveChef = async (details: ChefDetails) => {
   const { uuid, ...rest } = details;
 
-  return AppDataSource.transaction(
-    async (transaction) =>
-      await transaction.save(Chef, {
+  try {
+    return await AppDataSource.transaction(async (transaction) =>
+      transaction.save(Chef, {
         ...rest,
         ...(uuid !== undefined && { uuid }),
       })
-  );
+    );
+  } catch (error) {
+    if (error instanceof QueryFailedError && (error as any).code === "23505") {
+      const constraint = (error as any).constraint;
+      if (constraint === "UQ_44862ca93599a784f9f2cf72838") {
+        throw new HttpError(
+          "A chef with this information already exists. Please check for duplicate values.",
+          409
+        );
+      }
+    }
+
+    throw error;
+  }
 };
 
 const getAllChefs = async () => chefRepository.find({});
