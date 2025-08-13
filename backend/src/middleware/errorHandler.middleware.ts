f68d2d343c7a2a21d3700929365c7aba0880daa4
Infer-Noam <xd.NNoam39@idf.il>
2025-08-13 11:03:39 +0300

    Updated error handler so it only returns an http error

--- backend/src/middleware/errorHandler.middleware.ts
+++ backend/src/middleware/errorHandler.middleware.ts
@@ -1,4 +1,10 @@
+import { HttpError } from "@shared/types/httpError.type";
 import type { ErrorRequestHandler } from "express";
+import { BadRequestError } from "../utils/errors/badRequest.error";
+import { DuplicateError } from "../utils/errors/duplicate.error";
+import { InternalServerError } from "../utils/errors/internalServer.error";
+import { NotFoundError } from "../utils/errors/notFound.error";
+import { QueryFailedError } from "typeorm";
 
 const errorHandler: ErrorRequestHandler = (err, _, res, next) => {
   if (res.headersSent) {
@@ -6,8 +12,20 @@ const errorHandler: ErrorRequestHandler = (err, _, res, next) => {
   }
 
   const statusCode = err?.status ?? 500;
-  const message = err instanceof Error ? err.message : "Unknown error occurred";
-  res.status(statusCode).json({ message });
+
+  const httpErrorMap = new Map<number, HttpError>([
+    [400, new BadRequestError()],
+    [404, new NotFoundError()],
+    [409, new DuplicateError()],
+    [500, new InternalServerError()],
+  ]);
+
+  let httpError;
+
+  if (err instanceof HttpError) httpError = err;
+  else httpError = httpErrorMap.get(statusCode) ?? new InternalServerError();
+
+  res.status(httpError.status).json({ message: httpError.message });
 
   next();
 };
