992c9125137a3fc50813db9263b633af7f0f3896
Infer-Noam <xd.NNoam39@idf.il>
2025-08-13 20:13:04 +0300

    Updated allowed origins to use the the dev and prod ports

--- backend/src/config/allowedOrigins.ts
+++ backend/src/config/allowedOrigins.ts
@@ -1,3 +1,14 @@
-const allowedOrigins = ["http://localhost:5173"];
+const VITE_DEV_PORT = process.env.VITE_DEV_PORT;
+const VITE_PREVIEW_PORT = process.env.VITE_PREVIEW_PORT;
+
+const allowedOrigins: string[] = [];
+
+if (VITE_DEV_PORT) {
+  allowedOrigins.push(`http://localhost:${VITE_DEV_PORT}`);
+}
+
+if (VITE_PREVIEW_PORT) {
+  allowedOrigins.push(`http://localhost:${VITE_PREVIEW_PORT}`);
+}
 
 export default allowedOrigins;
