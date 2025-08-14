1fd75e07fecb6c07cd69d832dd0591a8489985b7
Infer-Noam <xd.NNoam39@idf.il>
2025-08-14 12:49:37 +0300

    Hoisted the http erros into a const file and used it

--- backend/src/middleware/errorHandler.middleware.ts
+++ backend/src/middleware/errorHandler.middleware.ts
@@ -1,13 +1,10 @@
 import { HttpError } from "@shared/types/httpError.type";
 import type { ErrorRequestHandler } from "express";
-import { BadRequestError } from "../utils/errors/badRequest.error";
-import { DuplicateError } from "../utils/errors/duplicate.error";
-import { InternalServerError } from "../utils/errors/internalServer.error";
-import { NotFoundError } from "../utils/errors/notFound.error";
 import {
   getHttpStatusEntry,
   HttpStatusCode,
 } from "@shared/enums/statusCodes/http-status-codes";
+import { HTTP_ERRORS } from "../utils/const/httpErrors.const";
 
 const errorHandler: ErrorRequestHandler = (err, _, res, next) => {
   if (res.headersSent) {
@@ -17,14 +14,7 @@ const errorHandler: ErrorRequestHandler = (err, _, res, next) => {
   const statusCode =
     getHttpStatusEntry(err?.status) ?? HttpStatusCode.INTERNAL_SERVER_ERROR;
 
-  const httpErrors: Record<HttpStatusCode, HttpError> = {
-    [HttpStatusCode.BAD_REQUEST]: new BadRequestError(),
-    [HttpStatusCode.NOT_FOUND]: new NotFoundError(),
-    [HttpStatusCode.DUPLICATE]: new DuplicateError(),
-    [HttpStatusCode.INTERNAL_SERVER_ERROR]: new InternalServerError(),
-  };
-
-  const httpError = err instanceof HttpError ? err : httpErrors[statusCode];
+  const httpError = err instanceof HttpError ? err : HTTP_ERRORS[statusCode];
 
   res.status(httpError.status).json({ message: httpError.message });
 
