ee23aab103b318ecd25e92efde55dd1553d9ad35
Infer-Noam <xd.NNoam39@idf.il>
2025-08-17 00:16:48 +0300

    Updated server logger to log at at tmp folder

--- backend/src/middleware/logger/index.ts
+++ backend/src/middleware/logger/index.ts
@@ -7,20 +7,19 @@ import requestLogger from "./request-logger.middleware";
 
 const fsPromises = fs.promises;
 
-// A builder function for a logger
+const logDir = "/tmp/logs";
+
 const createLogger = async (message: string, logName: string) => {
   const dateTime = `${format(new Date(), "yyyyMMdd\tHH:mm:ss")}`;
   const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
+
   try {
-    if (!fs.existsSync(path.join(__dirname, "..//..//..", "logs"))) {
-      await fsPromises.mkdir(path.join(__dirname, "..//..//..", "logs"));
+    if (!fs.existsSync(logDir)) {
+      await fsPromises.mkdir(logDir, { recursive: true });
     }
-    await fsPromises.appendFile(
-      path.join(__dirname, "..//..//..", "logs", logName),
-      logItem
-    );
+    await fsPromises.appendFile(path.join(logDir, logName), logItem);
   } catch (err) {
-    console.log(err);
+    console.error("Logger error:", err);
   }
 };
 
